4e5186f4c2f22de74b5ed2758fbfcefffd816480
EC2 Default User <ec2-user@ip-10-0-1-148.ec2.internal>
2025-09-01 10:43:56 +0000

    Fix ingress controller deployment

--- deployment-scripts/01-deploy-ingress.sh
+++ deployment-scripts/01-deploy-ingress.sh
@@ -2,6 +2,6 @@
 echo "Deploying NGINX Ingress Controller..."
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install my-release ingress-nginx/nginx-ingress --namespace ingress-nginx --create-namespace
+helm install my-release ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
 echo "NGINX Ingress Controller deployed successfully"
